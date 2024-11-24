d37a08b5c2ecb7ebcb447a64259ca70921dcf7ac
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-24 17:18:34 -0500

    Update Inicio.m

--- Inicio.m
+++ Inicio.m
@@ -6,7 +6,8 @@
 
 Different things: 
 - When you operate two numbers they are stored in a variable called Ans like calculators  
-- All variables are stored at the right section of the screen to be able to track their value 
+- All variables are stored at the right section of the screen to be able to track their value
+- Results are more accurate math wise, as for example, sqrt(-9) will display 0.0000 + 3.0000i 
 
 
 
@@ -15,12 +16,15 @@ FUNCTIONS:
 2. "cear" to delete all variables from the workspace
 3. "load" will charge a file .mat that stores varibles. Load file var, will only load this variable var 
 4. "clc" to clear the window as Crt + L of linux.
+5. "format" + long will increase the number of decimals, + short will lower this. 
 
 Math FUNCTIONS: 
 1. sin(x) 
+2. 
 
 
-
+SCRIPTS: 
+Is a way of mixing Microsoft Word + maths to visualize maths plus text, to create it New life script
 
 VARIABLES: 
 -Pi.   
