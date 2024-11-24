4d8031d6b2f7908ccc12f4af6e3c888be2ca7a89
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-24 17:10:16 -0500

    Update Inicio.m

--- Inicio.m
+++ Inicio.m
@@ -1,8 +1,28 @@
-% SIMILAR THINGS
+%{ SIMILAR THINGS
 -Same up arrow to move to previous commands used as Linux
 -Same asignation as could for variables as could be Python  
 
 
 
-% Different things: 
-When you operate two numbers they are stored in a variable called Ans like calculators  
+Different things: 
+- When you operate two numbers they are stored in a variable called Ans like calculators  
+- All variables are stored at the right section of the screen to be able to track their value 
+
+
+
+FUNCTIONS: 
+1. "save + filename : stores the file with the filename assigned. 
+2. "cear" to delete all variables from the workspace
+3. "load" will charge a file .mat that stores varibles. Load file var, will only load this variable var 
+4. "clc" to clear the window as Crt + L of linux.
+
+Math FUNCTIONS: 
+1. sin(x) 
+
+
+
+
+VARIABLES: 
+-Pi.   
+
+}%
