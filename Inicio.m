12ab86fef532997f3b74e362301d9ea4428c02b9
derianbv <116598387+derianbv@users.noreply.github.com>
2024-11-24 17:00:10 -0500

    Update Inicio.m

--- Inicio.m
+++ Inicio.m
@@ -1,1 +1,8 @@
+% SIMILAR THINGS
+-Same up arrow to move to previous commands used as Linux
+-Same asignation as could for variables as could be Python  
 
+
+
+% Different things: 
+When you operate two numbers they are stored in a variable called Ans like calculators  
